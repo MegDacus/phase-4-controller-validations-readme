275406580e3ace6fd74721f7039c80f6c277a6ba
MegDacus <124758848+MegDacus@users.noreply.github.com>
2023-06-19 15:32:30 +1000

    Added validation and error handling for bird :name

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,7 +10,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
@@ -22,29 +23,33 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
-    render json: bird
+    bird.update!(bird_params)
+    render json: bird, status: :accepted
   end
-
+  
   # DELETE /birds/:id
   def destroy
     bird = find_bird
     bird.destroy
     head :no_content
   end
-
+  
   private
-
+  
   def find_bird
     Bird.find(params[:id])
   end
-
+  
   def bird_params
     params.permit(:name, :species, :likes)
   end
-
+  
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+  
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
